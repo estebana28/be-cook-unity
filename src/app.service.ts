451048b45e542ae7d282314953f3ebb3534f3e04
Esteban Arce <eesteban.arce@gmail.com>
2024-05-25 19:17:27 -0300

    update version to fix deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! With Prisma and NeonDB';
+    return 'Hello World! V0.3';
   }
 }
